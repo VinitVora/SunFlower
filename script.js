c05a8c08a2e8a42178af5b6d670437678cf189b0
VinitVora <68478128+VinitVora@users.noreply.github.com>
2023-10-19 20:21:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+Shery.mouseFollower();
+
+Shery.makeMagnet(".magnet")
+
+Shery.textAnimate(".hvr", {
+    style: 2,
+})
+
+gsap.to(".aleftelm", {
+    scrollTrigger: {
+      trigger: "#aimages",
+      pin: true,
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1,
+});
+
+let sections = document.querySelectorAll(".aleftelm");
+Shery.imageEffect(".aimages", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
+
+Shery.imageEffect(".slideimg", {
+    style: 2,
+    config: {"a":{"value":2,"range":[0,30]},"b":{"value":0.7,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.279976851851852},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.4,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}} ,
+});
